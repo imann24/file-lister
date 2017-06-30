e5138d2a951c0e2f52d185bf4be980f03c017ee7
Isaiah Mann <contact@isaiahmann.com>
2017-06-29 22:08:11 -0700

    Print list of files

--- file_lister.py
+++ file_lister.py
@@ -2,6 +2,7 @@
 # Description: Used to list files, separated by comma in a directory
 
 import sys
+from os import walk
 
 # Constants
 no_ext = "--no-ext"
@@ -9,5 +10,17 @@ list_dir = "Listings"
 
 if(len(sys.argv) > 1):
     dir = sys.argv[1]
+    if(len(sys.argv) > 2):
+        remove_extensions = True
+    else:
+        remove_extensions
+    # Adapted from: https://stackoverflow.com/questions/3207219/how-do-i-list-all-files-of-a-directory
+    files = []
+    for(dirpath, dirnames, filenames) in walk(dir):
+        files.extend(filenames)
+        break
+    for i in range(0, len(files)):
+        files[i] = files[i].split(".")[0]
+    print(files)
 else:
     print("Please provide target directory")
